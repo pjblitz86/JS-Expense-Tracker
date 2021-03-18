1554fe8c9d9f1c8499c3c8bf6b4f7cc0e2428239
pjblitz86 <paulius.juknis@gmail.com>
2021-03-18 16:23:57 +0200

    persist add and delete to localStorage

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 },
-];
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 },
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 function addTransaction(e) {
   e.preventDefault();
@@ -30,6 +35,7 @@ function addTransaction(e) {
     addTransactionDOM(transaction);
 
     updateValues();
+    updateLocalStorage();
     text.value = '';
     amount.value = '';
   }
@@ -76,7 +82,13 @@ function updateValues() {
 
 function removeTransaction(id) {
   transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  updateLocalStorage();
   init();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
 function init() {
